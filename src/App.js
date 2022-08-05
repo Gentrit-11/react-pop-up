178d0e58b97a399b9c16d5fbf369148248e22099
GentritSheremetaj <79097057+Gentrit-11@users.noreply.github.com>
2022-08-05 17:09:08 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import Popup from "./components/Popup";
+import "./style.css";
+import { useState } from "react";
 
 function App() {
+  const [buttonPopup, setButtonPopup] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React popup</h1>
+      <br></br>
+      <button onClick={() => setButtonPopup(true)}>Open Popup</button>
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <div className="text">
+          <h3>Theme color</h3>
+          <a href="#">change theme</a>
+        </div>
+        <div className="all">
+          <div className="flex">
+            <h3>Font color</h3>
+          </div>
+          <div className="container">
+            <h4>#44444</h4>
+            <span className="background">jjjjj</span>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="all">
+          <div className="flex">
+            <h3>background color</h3>
+          </div>
+          <div className="container">
+            <h4>#2072EF</h4>
+            <span className="background2">jjjjj</span>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="all">
+          <div className="flex">
+            <h3>button color</h3>
+          </div>
+          <div className="container">
+            <h4>#2072EF</h4>
+            <span className="background3">jjjjj</span>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="all">
+          <div className="flex">
+            <h3>button border color</h3>
+          </div>
+          <div className="container">
+            <h4>#0053D1</h4>
+            <span className="background4">jjjjj</span>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="all">
+          <div className="flex">
+            <h3>button Mouseover color</h3>
+          </div>
+          <div className="container">
+            <h4>#0053D1</h4>
+            <span className="background5">jjjjj</span>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="buttons">
+          <button className="button1">cancel</button>
+          <button className="button2">save</button>
+        </div>
+      </Popup>
     </div>
   );
 }
